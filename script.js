1277205bc7a3cc4b98fbffdb29264b1fd3b9304b
MegaRaina606 <84876072+MegaRaina606@users.noreply.github.com>
2021-07-13 00:39:59 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,23 @@
+
+//var eles=document.getElementsByTagName('p');
+
+
+//console.log(eles[0]);
+
+//var ele=document.getElementsByClassName('para');
+//console.log(ele[2]);
+
+//var ele=document.getElementById('p1');
+//ele.innerText="LetsUpgrade";
+ 
+//function doSomething(){
+// var d=document.getElementById('p');
+// d.innerHTML="<h1>Hello</h1>";
+
+//}
+
+function takeValue(){
+  var inp=document.getElementById('inp');
+  var data=inp.value;
+  document.getElementById('name').innerText=data;
+}
